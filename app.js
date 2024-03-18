fd6b5247394c42d4adbaada012e1061b5e43173b
Shreya Agrawal <shreya.agrawal2121@gmail.com>
2024-03-18 23:41:08 +0530

    semantic search added

--- app.js
+++ app.js
@@ -2,13 +2,39 @@ const express = require('express');
 const mongoose = require('mongoose');
 const product = require('./models/productModel');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const axios = require('axios');
 const app = express();
+require('dotenv').config({ path: "./.env" });
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false})); 
 user_ = process.env.USERID;
 password_ = process.env.PASSWORD;
+openAIKey = process.env.OPENAI;
 const uri = `mongodb+srv://${user_}:${password_}@cluster0.aoju68d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+async function getEmbedding(query) {
+    // Define the OpenAI API url and key.
+    const url = 'https://api.openai.com/v1/embeddings';
+    const openai_key = openAIKey; // Replace with your OpenAI key.
+    
+    // Call OpenAI API to get the embeddings.
+    let response = await axios.post(url, {
+        input: query,
+        model: "text-embedding-3-small"
+    }, {
+        headers: {
+            'Authorization': `Bearer ${openai_key}`,
+            'Content-Type': 'application/json'
+        }
+    });
+    
+    if(response.status === 200) {
+        return response.data.data[0].embedding;
+    } else {
+        throw new Error(`Failed to get embedding. Status code: ${response.status}`);
+    }
+}
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -31,7 +57,7 @@ const client = new MongoClient(uri, {
   }
   run().catch(console.dir).then(() => {
     app.listen(5000, () => {
-        console.log('Server is running at port 3000');
+        console.log('Server is running at port 5000');
     });
   })
 
@@ -47,8 +73,46 @@ app.get('/movies', async(req, res) => {
     }catch(err){
         console.log('Error: ', err);
         res.status(500).json({error: err.message});
+
     }
 })
+app.get('/search_semantic', async(req, res) => {
+    try {
+        if (req.query.word) {
+            results = await client
+              .db("sample_mflix")
+              .collection("embedded_movies")
+              .aggregate([
+                {"$vectorSearch": {
+                  "queryVector": embedding,
+                  "path": "plot_embedding",
+                  "numCandidates": 100,
+                  "limit":100,
+                  "index": "vector_index",
+                    }},
+                {
+                  "$project": {
+                    "title": 1,
+                    "plot": 1,
+                    "score": {
+                      "$meta": "searchScore"
+                    }
+                  
+                  }
+                }
+                
+              ])
+              .toArray();
+    
+            return res.send(results);
+          
+        }
+    }
+        catch(err){
+            console.log('Error: ', err);
+            res.status(500).json({error: err.message});
+        }
+});
 app.get('/search_auto', async(req, res) => {
     try {
         if (req.query.word) {
@@ -87,6 +151,8 @@ app.get('/search_auto', async(req, res) => {
             res.status(500).json({error: err.message});
         }
 });
+
+
 /*
 
 app.post('/add_data', (req, res) =>{
